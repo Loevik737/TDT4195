76fe30d3c913b92e67cab55083f3743430ed7839
Håvard Aspen Løvik <hloevi@live.no>
2017-08-29 19:21:04 +0200

    drawing is now working

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -1,6 +1,23 @@
 // Local headers
 #include "program.hpp"
 #include "gloom/gloom.hpp"
+#include "gloom/shader.hpp"
+using namespace Gloom;
+
+
+unsigned int setUpVertexArrayObject()
+{
+	unsigned int array = 0;
+	glGenVertexArrays(1, &array);
+	return array;
+
+}
+unsigned int setUpVertexBufferObject()
+{
+	unsigned int buffer = 0;
+	glGenBuffers(1, &buffer);
+	return buffer;
+}
 
 
 void runProgram(GLFWwindow* window)
@@ -16,7 +33,38 @@ void runProgram(GLFWwindow* window)
     glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
 
     // Set up your scene here (create Vertex Array Objects, etc.)
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f, 
+		0.0f,  0.5f, 0.0f
+	};
 
+	unsigned int indices[] =
+	{
+		1, 2, 3
+	};
+
+	
+
+
+	unsigned int arrayID = setUpVertexArrayObject();
+	glBindVertexArray(arrayID);
+
+	glBindBuffer(GL_ARRAY_BUFFER, setUpVertexBufferObject());
+	//9 = array length
+	glBufferData(GL_ARRAY_BUFFER, (9*sizeof(float)) , &vertices, GL_STATIC_DRAW);
+	//stride is 0 because we only have one entry type in the vertex array
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, setUpVertexBufferObject());
+	//9 = array length
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 9 * sizeof(unsigned int), &indices, GL_STATIC_DRAW);
+
+	
+	Shader shader;
+	shader.makeBasicShader("C:/git/TDT4195/gloom/gloom/shaders/simple.vert","C:/git/TDT4195/gloom/gloom/shaders/simple.frag");
+	
     // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
@@ -24,6 +72,12 @@ void runProgram(GLFWwindow* window)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Draw your scene here
+		shader.activate();
+		glBindVertexArray(arrayID);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		
+
+
 
         // Handle other events
         glfwPollEvents();
