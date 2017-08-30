08e28664b24f269129fe3446c66a0055cf16ffb8
Håvard Aspen Løvik <hloevi@live.no>
2017-08-30 11:01:53 +0200

    can now draw n triangles

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -2,7 +2,6 @@
 #include "program.hpp"
 #include "gloom/gloom.hpp"
 #include "gloom/shader.hpp"
-using namespace Gloom;
 
 //function wor creating a VAO and returning its ID. A VAO is an object that links the Input buffers to shaders
 unsigned int setUpVertexArrayObject()
@@ -20,6 +19,29 @@ unsigned int setUpVertexBufferObject()
 	return buffer;
 }
 
+unsigned int sceneSetup(float* vertices, unsigned int* indices, unsigned int triangles, unsigned int indicesCount) {
+
+	//setting up a VAO and binding it witch can be compared with opening a textfile so you can write to it
+	unsigned int arrayID = setUpVertexArrayObject();
+	glBindVertexArray(arrayID);
+	//setting up a VBO witch is a space in the memory for holding the values for the vertices
+	glBindBuffer(GL_ARRAY_BUFFER, setUpVertexBufferObject());
+	//sending the vertices data to the GPU by telling OpenGl that the space must be sufficient for n(triangles*3) floatingpoint numbers corresponding to the vertices
+	// - ,giving it the vertices and tellign it that their values are going to be static since its not changing much(at all)
+	glBufferData(GL_ARRAY_BUFFER, (triangles * 9 )* sizeof(float), vertices, GL_STATIC_DRAW);
+	//stride is 0 because we only have one entry type in the vertex array, so we dont have to tell the number of bytes between each entery
+	//Pointer is 0 because it the entries starts at 0 in the buffer
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	//setting up a buffer for the indeces so we can tell opengl witch vertices who overlap and therefore can be drawn only one time
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, setUpVertexBufferObject());
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesCount * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+	return arrayID;
+}
+
+
 
 void runProgram(GLFWwindow* window)
 {
@@ -36,40 +58,41 @@ void runProgram(GLFWwindow* window)
     // Set up your scene here (create Vertex Array Objects, etc.)
 
 	float vertices[] = {
-		-0.6f, -0.6f, 0.0f,
-		0.6f, -0.6f, 0.6f,
-		0.0f,  0.0f, 0.0f,
-		-0.1f, -0.1f, 0.0f,
-		1.0f, -0.1f, 1.0f,
-		0.5f,  0.5f, 0.0f
+		-1.0f, -1.0f, 0.0f,
+		-0.5f, -1.0f, 0.6f,
+		-0.75f,  -0.25f, 0.0f,
+
+		-1.0f, 0.0f, 0.0f,
+		-0.5f, 0.0f, 0.6f,
+		-0.75f,  0.25f, 0.0f,
+
+		0.5f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.6f,
+		0.75f,  0.25f, 0.0f,
+
+		0.5f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.6f,
+		0.75f,  -0.25f, 0.0f,
+
+		-0.25f, -0.5f, 0.0f,
+		0.25f, -0.5f, 0.6f,
+		0.0f,  0.5f, 0.0f
+		
 	};
 
 	unsigned int indices[] =
 	{
-		0, 1, 2, 3, 4, 5
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
 	};
 
-	//setting up a VAO and binding it witch can be compared with opening a textfile so you can write to it
-	unsigned int arrayID = setUpVertexArrayObject();
-	glBindVertexArray(arrayID);
-	//setting up a VBO witch is a space in the memory for holding the values for the vertices
-	glBindBuffer(GL_ARRAY_BUFFER, setUpVertexBufferObject());
-	//sending the vertices data to the GPU by telling OpenGl that the space must be sufficient for n floatingpoint numbers corresponding to the vertices
-	// - ,giving it the vertices and tellign it that their values are going to be static since its not changing much(at all)
-	glBufferData(GL_ARRAY_BUFFER, (18*sizeof(float)) , &vertices, GL_STATIC_DRAW);
-	//stride is 0 because we only have one entry type in the vertex array, so we dont have to tell the number of bytes between each entery
-	//Pointer is 0 because it the entries starts at 0 in the buffer
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
-
-	//setting up a buffer for the indeces so we can tell opengl witch vertices who overlap and therefore can be drawn only one time
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, setUpVertexBufferObject());
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), &indices, GL_STATIC_DRAW);
+	const unsigned int indicesCount = 15;
+	const unsigned int triangles = 5;
+	unsigned int arrayID = sceneSetup(vertices, indices, triangles, indicesCount);
 
 	//making a shader object from the Shader.hpp file, and loading,linking and compiling the vert and frag shaders
-	Shader shader;
+	Gloom::Shader shader;
 	shader.makeBasicShader("C:/git/TDT4195/gloom/gloom/shaders/simple.vert","C:/git/TDT4195/gloom/gloom/shaders/simple.frag");
-	
+	shader.activate();
     // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
@@ -77,13 +100,9 @@ void runProgram(GLFWwindow* window)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Draw your scene here
-		shader.activate();
 		glBindVertexArray(arrayID);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
 		
-
-
-
         // Handle other events
         glfwPollEvents();
         handleKeyboardInput(window);
@@ -91,6 +110,7 @@ void runProgram(GLFWwindow* window)
         // Flip buffers
         glfwSwapBuffers(window);
     }
+	shader.deactivate();
 }
 
 
